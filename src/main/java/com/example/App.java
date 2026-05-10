07ab1aeda5c06bc5e6809bdc5825880852d4d43a
Tanish Gowda N <tanishgowda789@gmail.com>
2026-05-10 14:28:12 +0000

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -9,24 +9,31 @@ public class App {
 
     public static void main(String[] args) {
 
+        // Chrome configuration for Linux/Jenkins
         ChromeOptions options = new ChromeOptions();
-
         options.addArguments("--headless=new");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--disable-gpu");
 
-        // Selenium Manager will handle ChromeDriver automatically
+        // IMPORTANT:
+        // Do NOT set System.setProperty("webdriver.chrome.driver", ...)
+
         WebDriver driver = new ChromeDriver(options);
 
-        driver.get("https://www.saucedemo.com/");
+        try {
+            driver.get("https://www.saucedemo.com/");
 
-        driver.findElement(By.id("user-name")).sendKeys("standard_user");
-        driver.findElement(By.id("password")).sendKeys("secret_sauce");
-        driver.findElement(By.id("login-button")).click();
+            driver.findElement(By.id("user-name")).sendKeys("standard_user");
+            driver.findElement(By.id("password")).sendKeys("secret_sauce");
+            driver.findElement(By.id("login-button")).click();
 
-        System.out.println("Login successful");
+            System.out.println("Login successful");
 
-        driver.quit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            driver.quit();
+        }
     }
 }
